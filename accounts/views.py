0f816e701d657bc912c88e08b1e32a976c8b1b23
Ardhweb <sun.projectsdev@gmail.com>
2025-01-15 16:15:47 +0530

    update

--- accounts/views.py
+++ accounts/views.py
@@ -44,7 +44,10 @@ def register(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
-            return render(request,'accounts/register_done.html',{'new_user': new_user})
+            user = authenticate(request, username=user_form.cleaned_data['username'], password=user_form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+            return redirect("index")
     else:
         user_form = UserRegistrationForm()
     return render(request,'accounts/register.html',{'user_form': user_form})
